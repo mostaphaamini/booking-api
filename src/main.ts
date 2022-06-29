5c80d936dbc8d55b406b08d40c7a747620f3ef0b
mos <mostaphaamini@gmail.com>
2022-06-29 13:52:51 +0430

    Procfile

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  //await app.listen(process.env.PORT || 3000);
-  await app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d ", this.address().port);
-  });
+  await app.listen(process.env.PORT || 3000);
+  // await app.listen(process.env.PORT || 3000, function(){
+  //   console.log("Express server listening on port %d ", this.address().port);
+  // });
   
 }
 bootstrap();
